1a9e378899d29b2df71e06d202117f2e07c3a2cb
vivkananda Reddy <vivekanandapolu123@gmail.com>
2024-05-25 22:54:45 +0530

    added the src folder

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 
-import roleRoutes from "./Roles/role.route.js";
-import userRoutes from "./Users/user.route.js";
-import fileRoutes from "./File uploads/file.route.js";
+import roleRoutes from "./src/Roles/role.route.js";
+import userRoutes from "./src/Users/user.route.js";
+import fileRoutes from "./src/File uploads/file.route.js";
 
 import cors from "cors";
 import helmet from "helmet";
 
-import { uploadDir } from "./utils/multerConfig.js";
+import { uploadDir } from "./src/utils/multerConfig.js";
 
-import { verifyToken } from "./utils/auth.js";
+import { verifyToken } from "./src/utils/auth.js";
 
-import logger from "./middlewares/logger.js";
+import logger from "./src/middlewares/logger.js";
 
 const app = express();
 
